c9fe34162feaf764b420c0f3cd350940396e374b
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-03 14:32:52 -0800

    part 2!

--- 2022/aoc03/src/main.rs
+++ 2022/aoc03/src/main.rs
@@ -8,20 +8,39 @@ fn main() {
 
     let mut total_priority: u32 = 0;
 
-    contents.lines().for_each(|line| {
-        let first_half = &line[..line.len() / 2];
-        let second_half = &line[line.len() / 2..];
+    let mut iter = contents.lines();
 
-        let first_half_set: std::collections::HashSet<_> = first_half.chars().collect();
-        let second_half_set: std::collections::HashSet<_> = second_half.chars().collect();
+    // Loop through each line in the file, 3 at a time
+    loop {
+        let first = iter.next();
+        let second = iter.next();
+        let third = iter.next();
 
-        let intersection: Vec<_> = first_half_set.intersection(&second_half_set).collect();
+        // If we've reached the end of the file, break out of the loop
+        if first.is_none() || second.is_none() || third.is_none() {
+            break;
+        }
+
+        let first_char_set: std::collections::HashSet<_> =
+            first.expect("string is missing").chars().collect();
+        let second_char_set: std::collections::HashSet<_> =
+            second.expect("string is missing").chars().collect();
+        let third_char_set: std::collections::HashSet<_> =
+            third.expect("string is missing").chars().collect();
+
+        // Find the intersection of the three sets
+        let intersection1: std::collections::HashSet<_> =
+            first_char_set.intersection(&second_char_set).collect();
+        let intersection2: std::collections::HashSet<_> =
+            second_char_set.intersection(&third_char_set).collect();
+
+        let intersection: Vec<_> = intersection1.intersection(&intersection2).collect();
 
         // Get single character from intersection
         let character = intersection[0];
 
         // Convert char to char code
-        let character_code = *character as u8;
+        let character_code = **character as u8;
 
         let priority: u8;
         // Minus 96 for lowercase, minus 64 for uppercase
@@ -32,7 +51,7 @@ fn main() {
         }
 
         total_priority += priority as u32;
-    });
+    }
 
     println!("Total priority: {}", total_priority);
 }
