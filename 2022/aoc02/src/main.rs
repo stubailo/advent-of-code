e299888a172df4789dd826711927897094dfe239
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-02 17:12:06 -0800

    Finished part 2!

--- 2022/aoc02/src/main.rs
+++ 2022/aoc02/src/main.rs
@@ -1,5 +1,12 @@
 use std::fs;
 
+// Enum of rock, paper, scissors
+enum RPS {
+    Rock,
+    Paper,
+    Scissors,
+}
+
 fn main() {
     // Set file path to a string with "input.txt"
     let file_path = "input.txt";
@@ -29,28 +36,37 @@ fn main() {
         // Z: scissors
 
         // If we win, get 6 points, draw is 3 point, lose is 0 points
-        let mut line_score;
+        let mut line_score: i32 = 0;
+
+        let shape_to_play: RPS;
 
         // Use a match statement to determine the score
         match (words[0], words[1]) {
-            ("A", "X") => line_score = 3,
-            ("A", "Y") => line_score = 6,
-            ("A", "Z") => line_score = 0,
-            ("B", "X") => line_score = 0,
-            ("B", "Y") => line_score = 3,
-            ("B", "Z") => line_score = 6,
-            ("C", "X") => line_score = 6,
-            ("C", "Y") => line_score = 0,
-            ("C", "Z") => line_score = 3,
-            _ => line_score = 0,
+            ("A", "X") => shape_to_play = RPS::Scissors,
+            ("A", "Y") => shape_to_play = RPS::Rock,
+            ("A", "Z") => shape_to_play = RPS::Paper,
+            ("B", "X") => shape_to_play = RPS::Rock,
+            ("B", "Y") => shape_to_play = RPS::Paper,
+            ("B", "Z") => shape_to_play = RPS::Scissors,
+            ("C", "X") => shape_to_play = RPS::Paper,
+            ("C", "Y") => shape_to_play = RPS::Scissors,
+            ("C", "Z") => shape_to_play = RPS::Rock,
+            _ => panic!("Invalid input"),
         }
 
-        // Add the score for the move -- 1 for X, 2 for Y, 3 for Z
+        // Add the score for win/draw/lose
         line_score += match words[1] {
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            _ => 0,
+            "X" => 0,
+            "Y" => 3,
+            "Z" => 6,
+            _ => panic!("Invalid input"),
+        };
+
+        // Add the score for the move -- 1 for X, 2 for Y, 3 for Z
+        line_score += match shape_to_play {
+            RPS::Rock => 1,
+            RPS::Paper => 2,
+            RPS::Scissors => 3,
         };
 
         // Add the score for the line to the total score
