94f8b153f56814a34417b395c987ee622cd80426
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-04 20:56:11 -0800

    Updated starter code

--- 2022/aoc05/src/main.rs
+++ 2022/aoc05/src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
+use std::time::Instant;
 
 fn main() {
+    let start = Instant::now();
+
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut iter = contents.lines();
@@ -17,12 +20,13 @@ fn main() {
 
         let line = line.unwrap();
 
-        // print line
-        println!("{}", line);
-
         // add 1 to result
-        result += 1;
+        result += line.len();
     }
 
     println!("Result: {}", result);
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed is: {:?}", duration);
 }
