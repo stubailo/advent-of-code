77918549188eebde045e398111d081dc7e106337
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-04 21:25:06 -0800

    Working part 1 and 2

--- 2022/aoc05/src/main.rs
+++ 2022/aoc05/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fs;
 use std::time::Instant;
 
@@ -8,9 +9,16 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut iter = contents.lines();
 
-    let mut result = 0;
+    // Create a vector of vectors to hold a stack of characters
+    let mut stacks: Vec<Vec<char>> = Vec::new();
 
-    // Loop through each line in the file, 3 at a time
+    // Initialize with 9 vectors
+    for _ in 0..9 {
+        stacks.push(Vec::new());
+    }
+
+    let mut importing_initial_stacks = true;
+
     loop {
         let line = iter.next();
 
@@ -20,11 +28,71 @@ fn main() {
 
         let line = line.unwrap();
 
-        // add 1 to result
-        result += line.len();
+        if importing_initial_stacks {
+            for i in 0..9 {
+                let char = line.chars().nth(i * 4 + 1);
+
+                if char.is_none() {
+                    continue;
+                }
+
+                let char = char.unwrap();
+
+                if char == ' ' {
+                    continue;
+                }
+
+                // skip if char is a number
+                if char.is_numeric() {
+                    continue;
+                }
+
+                stacks[i].push(char);
+            }
+
+            if line.is_empty() {
+                importing_initial_stacks = false;
+                // reverse every vector in stacks
+                for stack in stacks.iter_mut() {
+                    stack.reverse();
+                }
+
+                continue;
+            }
+            continue;
+        }
+
+        // parse 3 numbers out of a line like "move 5 from 4 to 5"
+        let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+        let captures = re.captures(line).unwrap();
+
+        let num = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
+        let from = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let to = captures.get(3).unwrap().as_str().parse::<usize>().unwrap();
+
+        // pop num cards from from stack
+        let mut cards = Vec::new();
+        for _ in 0..num {
+            cards.push(stacks[from - 1].pop().unwrap());
+        }
+
+        // push cards to to stack
+        for card in cards.iter().rev() {
+            stacks[to - 1].push(*card);
+        }
     }
 
-    println!("Result: {}", result);
+    // print last item in each stack
+    for stack in stacks.iter() {
+        if stack.is_empty() {
+            continue;
+        }
+
+        let last = stack.last().unwrap();
+        print!("{}", last);
+    }
+
+    println!("");
 
     let duration = start.elapsed();
 
