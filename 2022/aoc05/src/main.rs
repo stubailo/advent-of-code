4b8afe492fedebc881a25e651b077d8e066ac49e
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-04 21:33:50 -0800

    Get rid of regex

--- 2022/aoc05/src/main.rs
+++ 2022/aoc05/src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use std::fs;
 use std::time::Instant;
 
@@ -62,13 +61,14 @@ fn main() {
             continue;
         }
 
-        // parse 3 numbers out of a line like "move 5 from 4 to 5"
-        let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
-        let captures = re.captures(line).unwrap();
-
-        let num = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-        let from = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        let to = captures.get(3).unwrap().as_str().parse::<usize>().unwrap();
+        // parse 3 numbers out of the line like "move 5 from 4 to 5"
+        let mut iter = line.split_whitespace();
+        iter.next(); // skip "move"
+        let num = iter.next().unwrap().parse::<usize>().unwrap();
+        iter.next(); // skip "from"
+        let from = iter.next().unwrap().parse::<usize>().unwrap();
+        iter.next(); // skip "to"
+        let to = iter.next().unwrap().parse::<usize>().unwrap();
 
         // pop num cards from from stack
         let mut cards = Vec::new();
