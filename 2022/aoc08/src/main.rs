08762f386ee926c4de862b57ce7dbb450c84e1cf
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-07 22:11:20 -0800

    Part 2

--- 2022/aoc08/src/main.rs
+++ 2022/aoc08/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut iter = contents.lines();
 
-    let mut num_visible = 0;
+    let mut max_score = 0;
 
     let mut trees: Vec<Vec<u32>> = Vec::new();
 
@@ -37,13 +37,15 @@ fn main() {
         for col_index in 0..row.len() {
             let tree_height = row[col_index];
 
-            if check_visible(&trees, row_index, col_index, tree_height) {
-                num_visible += 1;
+            let score = calc_scenic_score(&trees, row_index, col_index, tree_height);
+
+            if score > max_score {
+                max_score = score;
             }
         }
     }
 
-    println!("Num visible is: {}", num_visible);
+    println!("Max score is: {}", max_score);
 
     let duration = start.elapsed();
 
@@ -61,4 +63,51 @@ fn check_visible(
         || (col_index + 1..trees[row_index].len()).all(|i| trees[row_index][i] < tree_height)
         || (0..row_index).all(|i| trees[i][col_index] < tree_height)
         || (row_index + 1..trees.len()).all(|i| trees[i][col_index] < tree_height);
+}
+
+fn calc_scenic_score(
+    trees: &Vec<Vec<u32>>,
+    row_index: usize,
+    col_index: usize,
+    tree_height: u32,
+) -> u32 {
+    let mut visible_up = 0;
+    for i in (0..row_index).rev() {
+        visible_up += 1;
+        if trees[i][col_index] >= tree_height {
+            break;
+        }
+    }
+
+    let mut visible_down = 0;
+    for i in (row_index + 1)..trees.len() {
+        visible_down += 1;
+        if trees[i][col_index] >= tree_height {
+            break;
+        }
+    }
+
+    let mut visible_left = 0;
+    for i in (0..col_index).rev() {
+        visible_left += 1;
+        if trees[row_index][i] >= tree_height {
+            break;
+        }
+    }
+
+    let mut visible_right = 0;
+    for i in (col_index + 1)..trees[row_index].len() {
+        visible_right += 1;
+        if trees[row_index][i] >= tree_height {
+            break;
+        }
+    }
+
+    // print all of them
+    // println!(
+    //     "{}, {} ({}): up {}, down {}, left {}, right {}",
+    //     row_index, col_index, tree_height, visible_up, visible_down, visible_left, visible_right
+    // );
+
+    return visible_up * visible_down * visible_left * visible_right;
 }
