1f7b21a45736572aff4d3d227ad6ecb44e9febfc
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-11 22:12:58 -0800

    Optimize more by not making a vec

--- 2022/aoc12/src/main.rs
+++ 2022/aoc12/src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::fs;
 use std::time::Instant;
 
 #[derive(Copy, Clone, Eq, Hash, PartialEq)]
-struct Point {
+struct PointWithDistance {
     x: i32,
     y: i32,
+    steps: i32,
 }
 
 fn main() {
@@ -16,7 +18,11 @@ fn main() {
     let mut iter = contents.lines();
 
     let mut elevations: Vec<Vec<i32>> = Vec::new();
-    let mut ending_point: Point = Point { x: 0, y: 0 };
+    let mut ending_point: PointWithDistance = PointWithDistance {
+        x: 0,
+        y: 0,
+        steps: -1,
+    };
 
     let mut row_num = 0;
 
@@ -37,9 +43,10 @@ fn main() {
             if c == 'S' {
                 row.push(0);
             } else if c == 'E' {
-                ending_point = Point {
+                ending_point = PointWithDistance {
                     x: row_num,
                     y: col_num,
+                    steps: 0,
                 };
                 row.push(25);
             } else {
@@ -52,76 +59,69 @@ fn main() {
         row_num += 1;
     }
 
-    let mut num_steps = 0;
+    let num_steps;
 
-    let mut visited_locations: HashSet<Point> = HashSet::new();
-    let mut current_locations: Vec<Point> = Vec::new();
+    let mut visited_locations: HashSet<(i32, i32)> = HashSet::new();
+    let mut current_locations: VecDeque<PointWithDistance> = VecDeque::new();
 
     // Since we're looking for the zero elevation location that has the shortest path to the end,
     // we can start at the end and work backwards -- the first zero elevation location we find
     // will be the shortest path to the end.
-    visited_locations.insert(ending_point);
-    current_locations.push(ending_point);
+    visited_locations.insert((ending_point.x, ending_point.y));
+    current_locations.push_back(ending_point);
 
     'outer: loop {
-        num_steps += 1;
+        let loc = current_locations.pop_front().unwrap();
+        let next_locs = vec![
+            PointWithDistance {
+                x: loc.x + 1,
+                y: loc.y,
+                steps: loc.steps + 1,
+            },
+            PointWithDistance {
+                x: loc.x - 1,
+                y: loc.y,
+                steps: loc.steps + 1,
+            },
+            PointWithDistance {
+                x: loc.x,
+                y: loc.y + 1,
+                steps: loc.steps + 1,
+            },
+            PointWithDistance {
+                x: loc.x,
+                y: loc.y - 1,
+                steps: loc.steps + 1,
+            },
+        ];
 
-        // break outer if there are no more current locations
-        if current_locations.len() == 0 {
-            break 'outer;
-        }
-
-        let mut next_locations: Vec<Point> = Vec::new();
-
-        for loc in current_locations {
-            let next_locs = vec![
-                Point {
-                    x: loc.x + 1,
-                    y: loc.y,
-                },
-                Point {
-                    x: loc.x - 1,
-                    y: loc.y,
-                },
-                Point {
-                    x: loc.x,
-                    y: loc.y + 1,
-                },
-                Point {
-                    x: loc.x,
-                    y: loc.y - 1,
-                },
-            ];
-
-            for next_loc in next_locs {
-                if visited_locations.contains(&next_loc) {
-                    continue;
-                }
-
-                if next_loc.x >= elevations.len() as i32
-                    || next_loc.y >= elevations[0].len() as i32
-                    || next_loc.x < 0
-                    || next_loc.y < 0
-                {
-                    continue;
-                }
-
-                if elevations[next_loc.x as usize][next_loc.y as usize]
-                    < elevations[loc.x as usize][loc.y as usize] - 1
-                {
-                    continue;
-                }
-
-                if elevations[next_loc.x as usize][next_loc.y as usize] == 0 {
-                    break 'outer;
-                }
-
-                visited_locations.insert(next_loc);
-                next_locations.push(next_loc);
+        for next_loc in next_locs {
+            if visited_locations.contains(&(next_loc.x, next_loc.y)) {
+                continue;
             }
-        }
 
-        current_locations = next_locations;
+            if next_loc.x >= elevations.len() as i32
+                || next_loc.y >= elevations[0].len() as i32
+                || next_loc.x < 0
+                || next_loc.y < 0
+            {
+                continue;
+            }
+
+            if elevations[next_loc.x as usize][next_loc.y as usize]
+                < elevations[loc.x as usize][loc.y as usize] - 1
+            {
+                continue;
+            }
+
+            if elevations[next_loc.x as usize][next_loc.y as usize] == 0 {
+                num_steps = next_loc.steps;
+                break 'outer;
+            }
+
+            visited_locations.insert((next_loc.x, next_loc.y));
+            current_locations.push_back(next_loc);
+        }
     }
 
     println!("Num steps: {}", num_steps);
