9fe627b28bd3ec4cb95c6dc2322cb4b193055bcd
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-11 21:42:38 -0800

    Done

--- 2022/aoc12/src/main.rs
+++ 2022/aoc12/src/main.rs
@@ -1,13 +1,11 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
-struct Monkey {
-    items: Vec<u64>,
-    operation: String,
-    test_divisible_by: u64,
-    next_monkey_if_true: usize,
-    next_monkey_if_false: usize,
+#[derive(Copy, Clone, Eq, Hash, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
@@ -17,141 +15,139 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut iter = contents.lines();
 
-    // Start by parsing input, of the form:
-    // Monkey 0:
-    //   Starting items: 93, 98
-    //   Operation: new = old * 17
-    //   Test: divisible by 19
-    //     If true: throw to monkey 5
-    //     If false: throw to monkey 3
+    let mut elevations: Vec<Vec<i32>> = Vec::new();
 
-    let mut monkeys: Vec<Monkey> = Vec::new();
-    let mut monkey_index_to_num_inspected: HashMap<usize, u64> = HashMap::new();
+    let mut ending_point: Point = Point { x: 0, y: 0 };
 
-    'outer: loop {
-        let mut current_monkey = Monkey {
-            items: Vec::new(),
-            operation: String::new(),
-            test_divisible_by: 0,
-            next_monkey_if_true: 0,
-            next_monkey_if_false: 0,
-        };
+    let mut row_num = 0;
 
-        loop {
-            let line = iter.next();
+    loop {
+        let line = iter.next();
 
-            if line.is_none() {
-                monkeys.push(current_monkey);
+        if line.is_none() {
+            break;
+        }
+
+        let line = line.unwrap();
+
+        let mut col_num = 0;
+
+        // add line to elevations, translate a-z into numbers 0-25
+        let mut row: Vec<i32> = Vec::new();
+        for c in line.chars() {
+            if c == 'S' {
+                row.push(0);
+            } else if c == 'E' {
+                ending_point = Point {
+                    x: row_num,
+                    y: col_num,
+                };
+                row.push(25);
+            } else {
+                row.push(c as i32 - 'a' as i32);
+            }
+            col_num += 1;
+        }
+        elevations.push(row);
+
+        row_num += 1;
+    }
+
+    // Make a vector of all Points in elevations with elevation 0
+    let mut starting_points: Vec<Point> = Vec::new();
+    for i in 0..elevations.len() {
+        for j in 0..elevations[0].len() {
+            if elevations[i][j] == 0 {
+                starting_points.push(Point {
+                    x: i as i32,
+                    y: j as i32,
+                });
+            }
+        }
+    }
+
+    let mut min_num_steps = std::i32::MAX;
+
+    for starting_point in starting_points {
+        let mut num_steps = 0;
+        let mut found = false;
+
+        let mut visited_locations: HashSet<Point> = HashSet::new();
+        let mut current_locations: Vec<Point> = Vec::new();
+
+        visited_locations.insert(starting_point);
+        current_locations.push(starting_point);
+
+        'outer: loop {
+            num_steps += 1;
+
+            // break outer if there are no more current locations
+            if current_locations.len() == 0 {
                 break 'outer;
             }
 
-            let line = line.unwrap();
+            let mut next_locations: Vec<Point> = Vec::new();
 
-            if line.is_empty() {
-                monkeys.push(current_monkey);
-                continue 'outer;
-            } else if line.starts_with("Monkey") {
-                // do nothing
-            } else if line.trim().starts_with("Starting") {
-                line.split(": ").nth(1).unwrap().split(", ").for_each(|x| {
-                    current_monkey.items.push(x.parse::<u64>().unwrap());
-                });
-            } else if line.trim().starts_with("Operation") {
-                current_monkey.operation = line.split("= ").last().unwrap().to_string();
-            } else if line.trim().starts_with("Test") {
-                current_monkey.test_divisible_by =
-                    line.split(" ").last().unwrap().parse::<u64>().unwrap();
-            } else if line.trim().starts_with("If true") {
-                current_monkey.next_monkey_if_true =
-                    line.split(" ").last().unwrap().parse::<usize>().unwrap();
-            } else if line.trim().starts_with("If false") {
-                current_monkey.next_monkey_if_false =
-                    line.split(" ").last().unwrap().parse::<usize>().unwrap();
+            for loc in current_locations {
+                let next_locs = vec![
+                    Point {
+                        x: loc.x + 1,
+                        y: loc.y,
+                    },
+                    Point {
+                        x: loc.x - 1,
+                        y: loc.y,
+                    },
+                    Point {
+                        x: loc.x,
+                        y: loc.y + 1,
+                    },
+                    Point {
+                        x: loc.x,
+                        y: loc.y - 1,
+                    },
+                ];
+
+                for next_loc in next_locs {
+                    if visited_locations.contains(&next_loc) {
+                        continue;
+                    }
+
+                    if next_loc.x >= elevations.len() as i32
+                        || next_loc.y >= elevations[0].len() as i32
+                        || next_loc.x < 0
+                        || next_loc.y < 0
+                    {
+                        continue;
+                    }
+
+                    if elevations[next_loc.x as usize][next_loc.y as usize]
+                        > elevations[loc.x as usize][loc.y as usize] + 1
+                    {
+                        continue;
+                    }
+
+                    if next_loc == ending_point {
+                        found = true;
+                        break 'outer;
+                    }
+
+                    visited_locations.insert(next_loc);
+                    next_locations.push(next_loc);
+                }
+            }
+
+            current_locations = next_locations;
+        }
+
+        if found {
+            if num_steps < min_num_steps {
+                min_num_steps = num_steps;
             }
         }
     }
 
-    // multiply all of the test_divisible_by values
-    let mut test_divisible_by_product: u64 = 1;
-    for monkey in &monkeys {
-        test_divisible_by_product *= monkey.test_divisible_by;
-    }
-
-    // do 20 rounds of simulation
-    for _ in 0..10000 {
-        for i in 0..monkeys.len() {
-            let monkey = &mut monkeys[i];
-
-            // First item in the tuple is the monkey to give to, second is the worry level to give
-            let mut to_throw: Vec<(usize, u64)> = Vec::new();
-
-            for item in &monkey.items {
-                monkey_index_to_num_inspected
-                    .entry(i)
-                    .and_modify(|e| *e += 1)
-                    .or_insert(1);
-
-                // apply operation
-                let mut operation_tokens = monkey.operation.split_whitespace();
-
-                let operand_1 = operation_tokens.next().unwrap();
-                let operator = operation_tokens.next().unwrap();
-                let operand_2 = operation_tokens.next().unwrap();
-
-                let operand_1_value = if operand_1 == "old" {
-                    *item
-                } else {
-                    operand_1.parse::<u64>().unwrap()
-                };
-
-                let operand_2_value = if operand_2 == "old" {
-                    *item
-                } else {
-                    operand_2.parse::<u64>().unwrap()
-                };
-
-                let new_item_intermediate = match operator {
-                    "*" => operand_1_value * operand_2_value,
-                    "+" => operand_1_value + operand_2_value,
-                    "-" => operand_1_value - operand_2_value,
-                    "/" => operand_1_value / operand_2_value,
-                    _ => panic!("Unknown operator"),
-                };
-
-                let new_item = new_item_intermediate % test_divisible_by_product;
-
-                // now we determine which monkey to give to
-                let next_monkey = if new_item % monkey.test_divisible_by == 0 {
-                    monkey.next_monkey_if_true
-                } else {
-                    monkey.next_monkey_if_false
-                };
-
-                to_throw.push((next_monkey, new_item));
-            }
-
-            monkey.items = Vec::new();
-
-            for (monkey_to_throw_to, item) in to_throw {
-                monkeys[monkey_to_throw_to].items.push(item);
-            }
-        }
-    }
-
-    // get values of monkey_index_to_num_inspected
-    let mut monkey_index_to_num_inspected_values: Vec<u64> = Vec::new();
-    for (_, v) in monkey_index_to_num_inspected {
-        monkey_index_to_num_inspected_values.push(v);
-    }
-
-    // sort them
-    monkey_index_to_num_inspected_values.sort();
-    monkey_index_to_num_inspected_values.reverse();
-
-    // multiply the first 2
-    let answer = monkey_index_to_num_inspected_values[0] * monkey_index_to_num_inspected_values[1];
-
-    println!("Answer: {}", answer);
+    println!("Num steps: {}", min_num_steps);
 
     let duration = start.elapsed();
 
