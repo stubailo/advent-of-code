06e82605e337e30e1843089baf9af6c4bb91c40e
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-13 22:01:18 -0800

    Initial commit for 14

---
+++ 2022/aoc14/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut iter = contents.lines();
+
+    loop {
+        let line = iter.next();
+
+        if line.is_none() {
+            break;
+        }
+
+        let line = line.unwrap();
+
+        println!("{}", line);
+    }
+
+    println!("Time elapsed is: {:?}", start.elapsed());
+}
