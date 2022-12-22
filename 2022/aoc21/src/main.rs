6d4f8dca73ae252b2b6d4f8519940c5adfd7c3c8
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-22 16:34:01 -0700

    Part 2 solved

--- 2022/aoc21/src/main.rs
+++ 2022/aoc21/src/main.rs
@@ -7,7 +7,7 @@ use std::time::Instant;
 
 #[derive(Debug)]
 enum Monkey {
-    Val(u64),
+    Val(i64),
     Operation {
         input1: String,
         input2: String,
@@ -35,14 +35,18 @@ fn main() {
 
         let monkey_name = parts[0];
 
-        let monkey = match parts[1].parse::<u64>() {
+        let monkey = match parts[1].parse::<i64>() {
             Ok(val) => Monkey::Val(val),
             Err(_) => {
                 let parts: Vec<&str> = parts[1].split(" ").collect();
                 Monkey::Operation {
                     input1: parts[0].to_string(),
                     input2: parts[2].to_string(),
-                    operation: parts[1].chars().next().unwrap(),
+                    operation: if monkey_name == "root" {
+                        '-'
+                    } else {
+                        parts[1].chars().next().unwrap()
+                    },
                 }
             }
         };
@@ -50,62 +54,83 @@ fn main() {
         monkeys.insert(monkey_name.to_string(), monkey);
     }
 
-    let mut monkey_values: HashMap<String, u64> = HashMap::new();
+    // Manually found this
+    let max_humn = 5000000000000 as i64;
+    let mut lower_bound = 0;
+    let mut upper_bound = max_humn;
 
     loop {
-        let mut all_monkeys_have_values = true;
+        let middle = (lower_bound + upper_bound) / 2;
 
-        for (monkey_name, monkey) in &monkeys {
-            if monkey_values.contains_key(monkey_name) {
-                continue;
+        monkeys.insert("humn".to_string(), Monkey::Val(middle));
+
+        // print lower, upper, and middle
+        // println!("bounds: {} {} {}", lower_bound, middle, upper_bound);
+
+        let mut monkey_values: HashMap<String, i64> = HashMap::new();
+
+        loop {
+            let mut all_monkeys_have_values = true;
+
+            for (monkey_name, monkey) in &monkeys {
+                if monkey_values.contains_key(monkey_name) {
+                    continue;
+                }
+
+                let monkey_value = match monkey {
+                    Monkey::Val(val) => Some(*val),
+                    Monkey::Operation {
+                        input1,
+                        input2,
+                        operation,
+                    } => {
+                        let input1_value = monkey_values.get(input1);
+                        let input2_value = monkey_values.get(input2);
+
+                        if input1_value.is_none() || input2_value.is_none() {
+                            None
+                        } else {
+                            let input1_value = input1_value.unwrap();
+                            let input2_value = input2_value.unwrap();
+
+                            let monkey_value = match operation {
+                                '*' => input1_value * input2_value,
+                                '/' => input1_value / input2_value,
+                                '+' => input1_value + input2_value,
+                                '-' => input1_value - input2_value,
+                                _ => panic!("Unknown operation"),
+                            };
+
+                            Some(monkey_value)
+                        }
+                    }
+                };
+
+                if monkey_value.is_some() {
+                    monkey_values.insert(monkey_name.to_string(), monkey_value.unwrap());
+                } else {
+                    all_monkeys_have_values = false;
+                }
             }
 
-            let monkey_value = match monkey {
-                Monkey::Val(val) => Some(*val),
-                Monkey::Operation {
-                    input1,
-                    input2,
-                    operation,
-                } => {
-                    let input1_value = monkey_values.get(input1);
-                    let input2_value = monkey_values.get(input2);
-
-                    if input1_value.is_none() || input2_value.is_none() {
-                        None
-                    } else {
-                        let input1_value = input1_value.unwrap();
-                        let input2_value = input2_value.unwrap();
-
-                        let monkey_value = match operation {
-                            '*' => input1_value * input2_value,
-                            '/' => input1_value / input2_value,
-                            '+' => input1_value + input2_value,
-                            '-' => input1_value - input2_value,
-                            _ => panic!("Unknown operation"),
-                        };
-
-                        Some(monkey_value)
-                    }
-                }
-            };
-
-            if monkey_value.is_some() {
-                monkey_values.insert(monkey_name.to_string(), monkey_value.unwrap());
-            } else {
-                all_monkeys_have_values = false;
+            if all_monkeys_have_values {
+                break;
             }
         }
 
-        if all_monkeys_have_values {
+        let root_val = monkey_values.get("root").unwrap();
+
+        // println!("root_val: {}", root_val);
+
+        if root_val < &0 {
+            upper_bound = middle;
+        } else if root_val > &0 {
+            lower_bound = middle;
+        } else {
+            println!("Found it: {}", middle);
             break;
         }
     }
-
-    // print value of monkey "root"
-    println!(
-        "Value of monkey 'root': {}",
-        monkey_values.get("root").unwrap()
-    );
 
     println!("Time elapsed is: {:?}", start.elapsed());
 }
