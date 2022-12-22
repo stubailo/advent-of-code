ac234a319f674fb373589fa6ea17645b0bc4850b
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-22 11:02:14 -0700

    Initial commit for 21

---
+++ 2022/aoc21/src/main.rs
@@ -0,0 +1,26 @@
+#![allow(dead_code, unused)]
+
+use core::num;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    let mut iter = contents.lines();
+
+    loop {
+        let line = iter.next();
+
+        if line.is_none() {
+            break;
+        }
+
+        let line = line.unwrap();
+
+        println!("line: {}", line);
+    }
+
+    println!("Time elapsed is: {:?}", start.elapsed());
+}
