e534039dd234d4328661a55d7c457103ac587afd
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-22 17:30:50 -0700

    Part 1

--- 2022/aoc22/src/main.rs
+++ 2022/aoc22/src/main.rs
@@ -44,13 +44,6 @@ fn main() {
         y += 1;
     }
 
-    for row in (0..12) {
-        for col in (0..15) {
-            print!("{}", board.get(&(row, col)).unwrap_or(&' '));
-        }
-        println!();
-    }
-
     let line = iter.next().unwrap();
 
     let mut directions: Vec<Step> = Vec::new();
@@ -85,6 +78,94 @@ fn main() {
     }
 
     // done parsing
+
+    // find minimum col in row 0
+    let mut min_col = 100000;
+    for (row, col) in board.keys() {
+        if *row == 0 && *col < min_col {
+            min_col = *col;
+        }
+    }
+
+    let mut location: (i32, i32) = (0, min_col);
+    let mut direction: (i32, i32) = (0, 1);
+
+    // println!("Location: {:?}", location);
+
+    for step in directions {
+        match step {
+            Step::TurnLeft => {
+                if direction == (0, 1) {
+                    direction = (-1, 0);
+                } else if direction == (-1, 0) {
+                    direction = (0, -1);
+                } else if direction == (0, -1) {
+                    direction = (1, 0);
+                } else if direction == (1, 0) {
+                    direction = (0, 1);
+                }
+            }
+            Step::TurnRight => {
+                if direction == (0, 1) {
+                    direction = (1, 0);
+                } else if direction == (1, 0) {
+                    direction = (0, -1);
+                } else if direction == (0, -1) {
+                    direction = (-1, 0);
+                } else if direction == (-1, 0) {
+                    direction = (0, 1);
+                }
+            }
+            Step::Forward(distance) => {
+                for _ in 0..distance {
+                    let mut next_location = (location.0 + direction.0, location.1 + direction.1);
+
+                    if board.get(&next_location).is_none() {
+                        // get opposite direction
+                        let opposite_direction = (-direction.0, -direction.1);
+
+                        loop {
+                            next_location = (
+                                next_location.0 + opposite_direction.0,
+                                next_location.1 + opposite_direction.1,
+                            );
+
+                            if board.get(&next_location).is_none() {
+                                next_location = (
+                                    next_location.0 - opposite_direction.0,
+                                    next_location.1 - opposite_direction.1,
+                                );
+                                break;
+                            }
+                        }
+                    }
+
+                    if board.get(&next_location).unwrap() == &'#' {
+                        break;
+                    }
+
+                    location = next_location;
+
+                    // print location
+                    // println!("Location: {:?}", location);
+                }
+            }
+        }
+    }
+
+    let final_row = location.0 + 1;
+    let final_col = location.1 + 1;
+    let final_direction_score = match direction {
+        (0, 1) => 0,
+        (1, 0) => 1,
+        (0, -1) => 2,
+        (-1, 0) => 3,
+        _ => panic!("Invalid direction"),
+    };
+
+    let result = 1000 * final_row + 4 * final_col + final_direction_score;
+
+    println!("Result: {}", result);
 
     println!("Time elapsed is: {:?}", start.elapsed());
 }
