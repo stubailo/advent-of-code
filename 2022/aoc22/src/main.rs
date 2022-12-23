86bc02f90c85f51dff0be34b0acfdaded82bc841
Sashko Stubailo <s.stubailo@gmail.com>
2022-12-22 17:14:22 -0700

    Parsed

--- 2022/aoc22/src/main.rs
+++ 2022/aoc22/src/main.rs
@@ -5,11 +5,21 @@ use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 use std::time::Instant;
 
+#[derive(Debug, Clone, Copy)]
+enum Step {
+    TurnLeft,
+    TurnRight,
+    Forward(i32),
+}
+
 fn main() {
     let start = Instant::now();
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let mut iter = contents.lines();
 
+    let mut board: HashMap<(i32, i32), char> = HashMap::new();
+
+    let mut y = 0;
     loop {
         let line = iter.next();
 
@@ -19,8 +29,62 @@ fn main() {
 
         let line = line.unwrap();
 
-        println!("{}", line);
+        if line.is_empty() {
+            break;
+        }
+
+        for (i, c) in line.chars().enumerate() {
+            if c != '#' && c != '.' {
+                continue;
+            }
+
+            board.insert((y, i as i32), c);
+        }
+
+        y += 1;
     }
+
+    for row in (0..12) {
+        for col in (0..15) {
+            print!("{}", board.get(&(row, col)).unwrap_or(&' '));
+        }
+        println!();
+    }
+
+    let line = iter.next().unwrap();
+
+    let mut directions: Vec<Step> = Vec::new();
+
+    let mut iter = line.chars().peekable();
+    loop {
+        let char = iter.next();
+
+        if char.is_none() {
+            break;
+        }
+
+        let char = char.unwrap();
+
+        if char == 'L' {
+            directions.push(Step::TurnLeft);
+        } else if char == 'R' {
+            directions.push(Step::TurnRight);
+        } else if char.is_digit(10) {
+            let mut num = char.to_digit(10).unwrap() as i32;
+            loop {
+                let next_char = iter.peek();
+
+                if next_char.is_none() || !next_char.unwrap().is_digit(10) {
+                    directions.push(Step::Forward(num));
+                    break;
+                }
+
+                num = num * 10 + iter.next().unwrap().to_digit(10).unwrap() as i32;
+            }
+        }
+    }
+
+    // done parsing
 
     println!("Time elapsed is: {:?}", start.elapsed());
 }
