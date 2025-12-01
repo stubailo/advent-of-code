a3d99a539e317dd5d4b4957d8b73c32845ee2606
Sashko Stubailo <s.stubailo@gmail.com>
2025-11-30 21:55:23 -0800

    WIP

--- 2023/2023-3.ts
+++ 2023/2023-3.ts
@@ -7,49 +7,76 @@ const rows = inputFile
   // filter empty rows
   .filter((row) => row.trim());
 
-const spacesNextToPartNumbers = new Map<string, boolean>();
+const gearLocations = new Map<string, boolean>();
 
 rows.forEach((row, rowIndex) => {
   row.split("").forEach((char, charIndex) => {
     // if char is not . and not a digit
     if (char !== "." && !/\d/.test(char)) {
       // mark all spaces within 1
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          spacesNextToPartNumbers.set(
-            [rowIndex + i, charIndex + j].join(","),
-            true
-          );
-        }
-      }
+
+      gearLocations.set([rowIndex, charIndex].join(","), true);
     }
   });
 });
+
+const gearLocationToProduct = new Map<string, number>();
+const gearLocationToCount = new Map<string, number>();
 
 const numbers: number[] = [];
 let sum = 0;
 rows.forEach((row, rowIndex) => {
   let currNumber: string[] = [];
-  let currNumberIsAdjacent = false;
+  let gearLocation: string | null = null;
+
+  function save() {
+    if (currNumber.length > 0 && gearLocation) {
+      const num = parseInt(currNumber.join(""));
+
+      gearLocationToCount.set(
+        gearLocation,
+        (gearLocationToCount.get(gearLocation) || 0) + 1
+      );
+
+      if (gearLocationToProduct.has(gearLocation)) {
+        gearLocationToProduct.set(
+          gearLocation,
+          gearLocationToProduct.get(gearLocation)! * num
+        );
+      } else {
+        gearLocationToProduct.set(gearLocation, num);
+      }
+    }
+
+    currNumber = [];
+  }
+
   row.split("").forEach((char, charIndex) => {
     if (/\d/.test(char)) {
-      if (spacesNextToPartNumbers.get([rowIndex, charIndex].join(","))) {
-        currNumberIsAdjacent = true;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (gearLocations.get([rowIndex + i, charIndex + j].join(","))) {
+            gearLocation = [rowIndex + i, charIndex + j].join(",");
+          }
+        }
       }
 
       currNumber.push(char);
     } else {
-      if (currNumber.length > 0 && currNumberIsAdjacent) {
-        const num = parseInt(currNumber.join(""));
-        sum += num;
-        console.log(rowIndex, num, sum);
-      }
-
-      currNumber = [];
-      currNumberIsAdjacent = false;
+      save();
     }
   });
+
+  save();
 });
 
-// sum
-console.log(sum);
+// sum of all values of gearLocationToProduct, where gearLocationToCount === 2
+let answer = 0;
+gearLocationToProduct.forEach((product, gearLocation) => {
+  if (gearLocationToCount.get(gearLocation) === 2) {
+    console.log(gearLocation, product);
+    answer += product;
+  }
+});
+
+console.log(answer);
